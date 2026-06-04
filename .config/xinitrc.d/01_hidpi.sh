365f6b9700670e713e68bb753828255dd1cdeb5c
uptodawn <uptodawn@foxmail.com>
2026-06-05 04:47:33 +0800

    hidpi: fix global scale

--- .config/xinitrc.d/01_hidpi.sh
+++ .config/xinitrc.d/01_hidpi.sh
@@ -1,7 +1,7 @@
 SCALE=${SCALE:-1.0}
-echo "Xft.dpi: $(echo 96*$SCALE/${SCALE%.*}/1 | bc)" | xrdb -merge
+echo "Xft.dpi: $(echo 96*$SCALE/1 | bc)" | xrdb -merge
 export GDK_SCALE=${SCALE%.*}
-export GDK_DPI_SCALE=1
+export GDK_DPI_SCALE=$(printf "%.2f" $(echo "scale=2; 1/${SCALE%.*}" | bc))
 export QT_SCALE_FACTOR=$SCALE
 export QT_ENABLE_HIGHDPI_SCALING=1
 export QT_FONT_DPI=96
@@ -10,8 +10,11 @@ dbus-update-activation-environment --verbose --systemd \
 	GDK_SCALE GDK_DPI_SCALE QT_SCALE_FACTOR XCURSOR_SIZE \
 	QT_ENABLE_HIGHDPI_SCALING QT_FONT_DPI
 
-sed "s/@DUNST_FONT_SIZE@/$((${SCALE%.*}*11))/; s/@DUNST_SCALE@/${SCALE%.*}/" \
+sed "s/@DUNST_SCALE@/${SCALE%.*}/" \
 	$HOME/.config/dunst/dunstrc.in > $HOME/.config/dunst/dunstrc
 
-fcitxconf=$HOME/.config/fcitx5/conf/classicui.conf
-sed "s/@FCITX_FONT_SIZE@/$((${SCALE%.*}*11))/" $fcitxconf.in > $fcitxconf
+sed "s/@ROFI_DPI@/$(echo 96*$SCALE/1 | bc)/" \
+	$HOME/.config/rofi/config.rasi.in > $HOME/.config/rofi/config.rasi
+
+sed "s/@ELECTRON_SCALE@/$SCALE/" \
+	$HOME/.config/electron-flags.conf.in > $HOME/.config/electron-flags.conf
